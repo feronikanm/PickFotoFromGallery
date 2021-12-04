6551c7b09f632341fa4af4d4506ad158ba4a6c82
feronikanm <firaferonika27@gmail.com>
2021-12-05 00:03:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pickfotofromgallery/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.pickfotofromgallery
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pickfotofromgallery.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnImgPick.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE)
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery()
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery()
+            }
+
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000
+        //Permission code
+        private val PERMISSION_CODE = 1001
+    }
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+
+//    handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+
+            val uri : Uri = data?.data!!
+            val fileUri = data.data
+
+            binding.imgAuto.setImageURI(data.data)
+            binding.tvImageAuto.text = data.data.toString()
+            Log.d("Image Auto : ", (data.data).toString())
+
+            binding.imgUri.setImageURI(uri)
+            binding.tvImageUri.text = uri.toString()
+            Log.d("Image Uri : ", uri.toString())
+
+            binding.imgFileUri.setImageURI(fileUri)
+            binding.tvImageFileUri.text = fileUri.toString()
+            Log.d("Image FileUri : ", fileUri.toString())
+
+        }
+    }
+
+
+
+}
